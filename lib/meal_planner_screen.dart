81c14fda95c9b47be2785230700d7c6a41d24179
Amenakhatib <12332293@students.liu.edu.lb>
2025-11-26 17:34:05 +0200

    meal_planner_screen.dart

--- lib/meal_planner_screen.dart
+++ lib/meal_planner_screen.dart
@@ -145,15 +145,18 @@ String getCalorieUnit() {
     );
   }
 }
-List<Map<String, dynamic>> foodList = [
-  { "name": "Apple", "cal": 52, "count": 0,"image":
-  "https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YXBwbGV8ZW58MHx8MHx8fDA%3D" },
-  { "name": "Banana", "cal": 89, "count": 0,"image":
-  "https://media.istockphoto.com/id/1187668811/photo/fresh-bananas-on-wooden-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=yoA-t9KI57DflISTnpUdgOJM7yZapLPsT3u7aFqYymE=" },
-  { "name": "Chicken", "cal": 200, "count": 0 ,"image":
-  "https://images.unsplash.com/photo-1606728035253-49e8a23146de?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2hpY2tlbnxlbnwwfHwwfHx8MA%3D%3D"},
-  { "name": "Rice", "cal": 80, "count": 0,"image":
-  "https://images.unsplash.com/photo-1536304993881-ff6e9eefa2a6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmljZXxlbnwwfHwwfHx8MA%3D%3D" },
-  { "name": "Oil", "cal": 1, "count": 0,"image":
+ist<Map<String, dynamic>> foodList = [
+  { "name": "Fruits & Vegetables", "cal": 52, "count": 0,"image":
+  "https://plus.unsplash.com/premium_photo-1664302148512-ddea30cd2a92?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZnJ1aXRzJTIwYW5kJTIwdmVnZXRhYmxlfGVufDB8fDB8fHww" },
+  { "name": "Meats & Chickens", "cal": 89, "count": 0,"image":
+  "https://plus.unsplash.com/premium_photo-1724260604247-589515ec1246?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fG1lYXRzJTIwJTI2JTIwY2hpY2tlbiUyMGdyb3VwfGVufDB8fDB8fHww" },
+  { "name": "Diary products", "cal": 200, "count": 0 ,"image":
+  "https://plus.unsplash.com/premium_photo-1663127123513-a11369f67c8c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWlsayUyMHByb2R1Y3RzfGVufDB8fDB8fHww"},
+  { "name": "Cereals(starch)", "cal": 80, "count": 0,"image":
+  "https://plus.unsplash.com/premium_photo-1733317211709-c6c6c13012f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YnJlYWQlMjBwcm9kdWN0c3xlbnwwfHwwfHx8MA%3D%3D" },
+  { "name": "Oil & Fats", "cal": 1, "count": 0,"image":
   "https://media.istockphoto.com/id/1206682746/photo/pouring-extra-virgin-olive-oil-in-a-glass-bowl.webp?a=1&b=1&s=612x612&w=0&k=20&c=O3qhyYvzGTBTikWGySIiNveiCqwxgwS3nQ9Hem2o_FU=" },
+  { "name": "Candies & sugar", "cal": 200, "count": 0 ,"image":
+  "https://images.unsplash.com/photo-1621939514649-280e2ee25f60?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fENhbmRpZXMlMjAlMjYlMjBjaG9jb2xhdGUlMjBwcm9kdWN0c3xlbnwwfHwwfHx8MA%3D%3D"},
+
 ];
